c4331873348cfcaa4d04a0a7bfb10fdc9bed1a3e
Ali Vaghey <a.vaghey0@gmail.com>
2026-02-17 18:06:10 +0330

    imp Calculator

---
+++ Calculator.php
@@ -0,0 +1,20 @@
+<?php
+
+$x = readline("Please enter the first number :");
+
+$y = readline("Please enter the second number :");
+
+echo "\n";
+echo $x + $y;
+echo "\n\n";
+
+//---------------------------------------
+
+$z = $x - $y;
+echo $x / $y;// تقسیم
+echo "\n";
+echo $x % $y; //باقی مانده
+echo "\n";
+echo $x * $y; //ضرب
+echo "\n";
+echo $x ** $y;
